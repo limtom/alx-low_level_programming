354fbbd30866516866039faf5d6c2b5c753ec8b1
limtom <horlartom@live.com>
2022-06-05 16:28:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,11 @@
  *Using the puts function
  */
 #include<stdio.h>
-int main(void)/*main function*/
+int main(void)
+/**
+ *main function
+ *returns 0
+*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
