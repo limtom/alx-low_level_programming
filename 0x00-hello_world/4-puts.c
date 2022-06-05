fe8584ab363729aab4bb705dec7f1da502a3e6ac
limtom <horlartom@live.com>
2022-06-05 16:13:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-int main(void)
+int main(void)/*main function*/
 {
-puts("\"Programming is like building a multilingual puzzle \n");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
