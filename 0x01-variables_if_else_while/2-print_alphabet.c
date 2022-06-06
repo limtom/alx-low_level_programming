38feeb8ef20b82186606aa770ae47576d96fd9ac
limtom <horlartom@live.com>
2022-06-06 13:57:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Main - The main entry
+ * Description: This program prints the alphabet in lowercase
+ *
+ * Return : Success
+ */
+
+int main(void)
+{
+char alpaha;
+for (alpha = 'a'; alpha<='z'; alpha++)
+{
+  putchar(alpha)
+}
+ return (0)
+}
