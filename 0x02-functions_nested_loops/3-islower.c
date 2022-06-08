05c12e46d492778bbc35633c8658891cd00ea590
limtom <horlartom@live.com>
2022-06-07 20:58:22 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+/**
+ * Description - Write a function that checks for lowercase character.
+ *
+ *Return 0 or 1
+ */
+
+int _islower(int c)
+{
+char c = "c";
+int res = islower(c);
+_putchar(res);
+return (0);
+}
