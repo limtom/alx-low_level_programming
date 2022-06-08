832c2927a42edbc1b3fca3c59b33926d6cc50550
limtom <horlartom@live.com>
2022-06-08 15:01:12 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - compute absolute value of a number
+ * Description: compute the absolute value of a number
+ * Return: 0 success
+ */
+
+int _abs(int)
+{
+int n;
+return (abs(n));
+}
