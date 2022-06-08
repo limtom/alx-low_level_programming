8cf8eab52ab2063f66ad88592243cdede6185e47
limtom <horlartom@live.com>
2022-06-08 16:23:05 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  *
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer starting from 00:00 to 23:59
+ *
  */
 void jack_bauer(void)
 {
@@ -22,5 +22,5 @@ _putchar('\n');
 j++;
 }
 i++;
-}  
+}
 }
