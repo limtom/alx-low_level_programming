e128730ec5eee5a59571553c2d1327c7f3e63159
limtom <horlartom@live.com>
2022-06-08 14:16:03 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign: print the sign of a number
+ * @n: char variable
+ * Return: returns 1 and + if n > 1 and other wise
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');  
+return (0);
+}
+ else
+_putchar('-');
+return (-1);
