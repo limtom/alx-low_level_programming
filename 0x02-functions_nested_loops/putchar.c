745e36658306e5e10b332c03fa7c6e46b8d0374c
limtom <horlartom@live.com>
2022-06-08 14:40:09 -0700

    5-sign.c

--- 0x02-functions_nested_loops/putchar.c
+++ 0x02-functions_nested_loops/putchar.c
@@ -1,7 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int _putchar(int ch)
+/**
+ * _putchar - writes the character c to standard output
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-putchar(ch);
-return (0);
+return (write(1, &c, 1));
 }
