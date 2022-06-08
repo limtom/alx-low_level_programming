f978d6f048d8d6c31af84849043b72b0919f3ca4
limtom <horlartom@live.com>
2022-06-07 19:18:17 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,10 @@
-#include "main.h"
-  
+#include "main.h" 
 #include <stdio.h>
-
 /**
  *main -  prints _putchar, followed by a new line.
  *
  *Description: don't use any of the standard library.
- *return: 0
+ *Return: 0
  */
 int main(void)
 {
@@ -14,7 +12,7 @@ int charArr[] = {95, 112, 117, 116, 99, 104, 97, 114};
 int i = 0;
 while (i < 8)
 {
-  _putchar(charArr[i]);
+_putchar(charArr[i]);
 i++;
 }
 putchar(10);
