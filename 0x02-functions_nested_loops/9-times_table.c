5f6604d338b75180c5f1038b256615f8bf087acf
limtom <horlartom@live.com>
2022-06-08 18:39:55 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 int i, j, k;
 for (i = 0; i < 10; i++)
 {
-  for (j = 0; j < 10; j++)
+for (j = 0; j < 10; j++)
 {
 k = j * i;
 if (j == 0)
@@ -24,7 +24,7 @@ _putchar(k + '0');
 } else if (k >= 10)
 {
 _putchar(',');
-_putchar(' ');   
+_putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
