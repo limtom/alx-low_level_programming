c193ab88970e60154010f29890f951093f31570d
limtom <horlartom@live.com>
2022-06-08 17:36:47 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,10 +7,18 @@
 
 void print_to_98(int n)
 {
-int i = n;
-while (n <= 98)
+if (n >= 98)
 {
-printf("%i, ", i);
-i++
+while (n > 98)
+printf("%d, ", n);
+n--;
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n);
+n++
+printf("%d\n", n);
 }
 }
