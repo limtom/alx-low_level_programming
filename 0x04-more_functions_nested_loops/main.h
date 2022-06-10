c4d2fb931a4121eae1f1d41fc208c4a81e3a7464
limtom <horlartom@live.com>
2022-06-09 22:20:16 -0700

    fixed

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int);
 int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
