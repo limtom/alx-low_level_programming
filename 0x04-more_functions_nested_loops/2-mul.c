bf6e30edcd2b227edb79d3f686a625e83ff55627
limtom <horlartom@live.com>
2022-06-09 21:56:40 -0700

    some

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - Multiply two given integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Return product of two ints
+ */
+int mul(int a, int b)
+{ 
+return (a * b); 
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Print numbers from 0 - 9
+ *
+ * Return: Nothing
+ */
+void print_numbers(void)  
+{
+int n;
+n = 0;
+while (n <= 9)
+{
+_putchar(n + '0');
+n++;
+} 
+_putchar('\n'); 
+}
