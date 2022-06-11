713cb7d0e4644fc3fbd7c566d193f81d8b8ca0cd
limtom <horlartom@live.com>
2022-06-10 21:56:32 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-#include "main.h"
 /**
- * fizz_buzz- print numbers between 1 and 100 and replace multiples
+ * Main- entry
+ * Description: fizz_buzz print numbers between 1 and 100 and replace multiples
  * of 3  with fizz and multiples of 5 with buzz while multiples of
  * of 3 and 5 with fizzbuzz
  * Return: 0
  */
-int fizzbuzz(void)
+int main(void)
 {
-int i;
-char f = "fizz";
-char b = "buzz";
-char fb = "fizzbuzz";
-for (i = 1; i <= 100; i++)
-{
-if (i % 15 == 0)
-printf("%s ",fb);
-else if (i % 5 == 0)
-printf("%s ",b);
-else if (i % 3 == 0)
-printf("%s ",f);
-else
-printf("%i ", i);
-}
-printf("\n");
-return (0);
-}
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
