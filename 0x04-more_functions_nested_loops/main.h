637a82291de9642a48ceb42d9b7c6db9f5eef21b
limtom <horlartom@live.com>
2022-06-10 22:09:26 -0700

    fizz_buzz

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,5 @@ void more_numbers(void);
 void print_line(int);
 void print_diagonal(int);
 void print_square(int size);
-int fizzbuzz(void);
 void print_triangle(int size);
 void print_number(int n);
