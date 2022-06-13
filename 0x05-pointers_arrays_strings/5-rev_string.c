8f4000d77c7c42f11c4ad75cc88a4bc713b67777
limtom <horlartom@live.com>
2022-06-13 23:52:23 +0000

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,10 +10,10 @@ void rev_string(char *s)
 {
 int len, i, temp;
 len = strlen(s);
-for (i = 0; i < len/2; i++)
+for (i = 0; i < len / 2; i++)
 {
 temp = s[i];
-s[i] = s[len-i-1];
-s[len-i-1] = temp;
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
 }
 }
