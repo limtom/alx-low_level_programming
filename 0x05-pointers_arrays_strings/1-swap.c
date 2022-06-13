db390fbd53d44f1b34fb60962af93d6afcf9c8d8
limtom <horlartom@live.com>
2022-06-13 19:41:39 +0000

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* swap_int- swap the value of two integer
+* @a: first integer parameter
+* @b: second integer parameter
+* Return: Success 0
+*/
+
+void swap_int(int *a, int *b)
+{
+*a = *b;
+*b = *a;
+}
