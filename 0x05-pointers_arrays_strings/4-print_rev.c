7a1d909c58d11258336fa5c6cbc96a5183d28472
limtom <horlartom@live.com>
2022-06-13 22:39:23 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* print_rev- Print a string in reverse
+* @s: a character parameter to reverse
+* Return: the reverse string
+*/
+void print_rev(char *s)
+{
+char str[500];
+int len, i;
+strcpy(str, s);
+len = strlen(str);
+for (i = len-1; i >= 0; i--)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+{
