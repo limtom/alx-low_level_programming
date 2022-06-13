3a02bc93fb2bf182bca86164c69d56bf40973be5
limtom <horlartom@live.com>
2022-06-13 21:28:20 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* strlen- Print the length of a string
+* @s: The character parmeter
+* Return: An integer which is the length;
+*/
+
+int _strlen(char *s)
+{
+char str[100];
+int len;
+strcpy(str,s);
+len = strlen(str);
+return (len);
+}
