5cc04bbb61a73035724f912dbf18a3b76b549c19
limtom <horlartom@live.com>
2022-06-14 21:28:49 +0000

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/**
+ * Main- Entry point
+  * 101-crackme: generate a random string
+   * Return: 0 success
+    */
+    
+    int main() {
+        //Create the variable 
+            int i, str_length;
+                char str[] = "abcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*_+-;:/|";
+                    
+                        //Allocate a memory space for the password
+                            char *password = malloc(sizeof(char)*10);
+                                
+                                    //Get the length of the string
+                                        str_length = strlen(str);
+                                            
+                                                srand(time(NULL)); //Set the seed for the random generator
+                                                    
+                                                        //Generate the random string 
+                                                            for(i = 0; i < 10; i++){
+                                                                    int ch_index = rand()%str_length;//Number generated between the length of the character
+                                                                            password[i] = str[ch_index];
+                                                                                }
+                                                                                    
+                                                                                        //Print the password
+                                                                                            for(i = 0; i < strlen(password); i++){
+                                                                                                    printf("%c", password[i]);
+                                                                                                        }
+                                                                                                            
+                                                                                                                //Free the allocated memeory
+                                                                                                                    free(password);
+                                                                                                                        
+                                                                                                                            return 0;
+                                                                                                                            }
