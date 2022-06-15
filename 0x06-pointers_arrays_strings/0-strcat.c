91f73dd05801e3d91226b79c06715885779a70bd
limtom <horlartom@live.com>
2022-06-15 21:42:49 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,8 +8,16 @@
 * Return: combine string containing the dest + src
 */
 
-char _strcat(char *dest, char *src)
-{
-strcat(dest, src);
-return (dest);
-}
+char *_strcat(char *dest, char *src){
+    if(dest == NULL) return NULL;
+        int i = 0;
+            /*get the length of dest of te dest*/
+                int len = strlen(dest);
+                    while(src[i] != '\0'){
+                            dest[len++] = src[i];
+                                    i++;
+                                            if(src[i] == '\0')
+                                                    dest[len++] = '\0';
+                                                        }
+                                                            return (dest);
+                                                            }
