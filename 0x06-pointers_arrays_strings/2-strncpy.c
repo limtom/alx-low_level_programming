024aedeab5e78544a481428b50dceb7a6b85e15e
limtom <horlartom@live.com>
2022-06-17 13:31:33 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _strncpy- copy a string with number of string
+ * @dest: the destination to copy string
+ * @src: string to copy
+ * @n: number of string to copy
+ * Return: the destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+char *res = dest;
+while (*src != "\0" && n--)
+*dest++ = *src++;
+*dest = "\0";
+return (dest);
+}
