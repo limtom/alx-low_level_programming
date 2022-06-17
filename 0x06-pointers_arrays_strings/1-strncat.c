36e9703ffb6d6eb8b5dbe4b712bf3ca04ac5c331
limtom <horlartom@live.com>
2022-06-17 03:29:43 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,9 @@ char *_strncat(char *dest, char *src, int n)
 {
 char *res = dest;
 while (*dest != '\0')
-{
 dest++;
-}
 while (*src != '\0' && n--)
-{
 *dest++ = *src++;
-}
 *dest = '\0';
 return (res);
 }
