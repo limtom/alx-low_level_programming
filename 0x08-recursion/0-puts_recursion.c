f6c5d0db17f25881f392955611e01adde42d3bf4
limtom <horlartom@live.com>
2022-06-23 13:17:06 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * puts_recursion- A function that print a string
+ *@s: string
+ */
+
+void puts_recursion(char *s)
+{
+if (*s == '\0')
+_putchar('\n');
+else
+{
+_putchar(*s);
+puts_recursion(s + 1);
+}
+}
