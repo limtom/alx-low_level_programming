b0dcd4ab806ac8376f8bf197609fc540c8ba9dc1
limtom <horlartom@live.com>
2022-06-26 10:36:43 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main- print number of arguments
+ *@argc: argument count
+ *@argv: arguments vector
+ *
+ *Return: zero orsuccess
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%d\n", argc);
+return (0);  
+}
