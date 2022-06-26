80cc39d701cb17bd954182bbba9c23095795335b
limtom <horlartom@live.com>
2022-06-26 13:34:41 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 {
 int i, j;
 int res = 0;
-char *arg;
+char *a;
 if (argc < 2)
 {
 printf("0\n");
@@ -23,16 +23,16 @@ else
 {
 for (i = 1; i < argc; i++)
 {
-arg = argv[i];
-for (j = 0; j < strlen(arg); j++)
+a = argv[i];
+for (j = 0; j < strlen(a); j++)
 {
-if (isdigit(arg[j]) == 0)
+if (isdigit(a[j]) == 0)
 {
 printf("Error\n");
 return (1);
 }
-res += atoi(arg);
- arg++;
+res += atoi(a);
+a++;
 }
 }
 printf("%d\n", res);
